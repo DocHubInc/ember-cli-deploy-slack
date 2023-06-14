c3be43d3db33001d992b5b81ad44aba395b66ede
Bryan Pierce <bryanpierce@dochub.com>
2023-06-14 12:42:48 -0500

    Use slack-notify instead of node-slackr package.

--- tests/slack-test.js
+++ tests/slack-test.js
@@ -1,36 +1,26 @@
 /*eslint-env node*/
-var expect        = require('chai').expect;
-var sinon         = require('sinon');
-var Slack         = require('node-slackr');
-var SlackNotifier = require('../lib/slack-notifier.js');
+var expect = require("chai").expect;
+var SlackNotifier = require("../lib/slack-notifier.js");
 
-var WEBHOOK_URL = 'https://hooks.slack.com/services/123123';
-var CHANNEL     = '#testing';
-var USER_NAME   = 'ember-cli-deploy';
+var WEBHOOK_URL = "https://hooks.slack.com/services/123123";
+var CHANNEL = "#testing";
+var USER_NAME = "ember-cli-deploy";
 
-var slackNotify;
-
-describe('SlackNotifier', function() {
+describe("SlackNotifier", function () {
   var slack;
-  var sandbox;
 
-  beforeEach(function() {
+  beforeEach(function () {
     slack = new SlackNotifier({
       enabled: true,
       webhookURL: WEBHOOK_URL,
       channel: CHANNEL,
-      username: USER_NAME
+      username: USER_NAME,
     });
-
-    sandbox = sinon.sandbox.create();
-    slackNotify = sandbox.spy(Slack.prototype, 'notify');
   });
 
-  afterEach(function() {
-    sandbox.restore();
-  });
+  afterEach(function () {});
 
-  it('it can be initialized', function() {
+  it("it can be initialized", function () {
     expect(slack).to.be.ok;
     expect(slack.enabled).to.eql(true);
     expect(slack.webhookURL).to.eql(WEBHOOK_URL);
@@ -38,61 +28,51 @@ describe('SlackNotifier', function() {
     expect(slack.username).to.eql(USER_NAME);
   });
 
-  describe('#_getSlackOptions', function() {
-    it("snake_cases and adds `iconURL` and/or `iconEmoji` options if they are set", function() {
+  describe("#_getSlackOptions", function () {
+    it("snake_cases and adds `iconURL` and/or `iconEmoji` options if they are set", function () {
       var iconEmojiOnly = new SlackNotifier({
         enabled: true,
         webhookURL: WEBHOOK_URL,
         channel: CHANNEL,
         username: USER_NAME,
-        iconEmoji: ':dromedary_camel:'
+        iconEmoji: ":dromedary_camel:",
       });
       var bothOptions = new SlackNotifier({
         enabled: true,
         webhookURL: WEBHOOK_URL,
         channel: CHANNEL,
         username: USER_NAME,
-        iconEmoji: ':dromedary_camel:',
-        iconURL: 'http://placehold.it/128x128'
+        iconEmoji: ":dromedary_camel:",
+        iconURL: "http://placehold.it/128x128",
       });
 
       expect(slack._getSlackOptions()).to.eql({
         channel: CHANNEL,
-        username: USER_NAME
+        username: USER_NAME,
       });
       expect(iconEmojiOnly._getSlackOptions()).to.eql({
         channel: CHANNEL,
         username: USER_NAME,
-        icon_emoji: ':dromedary_camel:'
+        icon_emoji: ":dromedary_camel:",
       });
       expect(bothOptions._getSlackOptions()).to.eql({
         channel: CHANNEL,
         username: USER_NAME,
-        icon_emoji: ':dromedary_camel:',
-        icon_url: 'http://placehold.it/128x128'
+        icon_emoji: ":dromedary_camel:",
+        icon_url: "http://placehold.it/128x128",
       });
     });
   });
 
-  describe('enabled', function() {
-    describe('#notify', function() {
-      it('is callable', function() {
-        expect(typeof(slack.notify)).to.eql('function');
+  describe("enabled", function () {
+    describe("#notify", function () {
+      it("is callable", function () {
+        expect(typeof slack.notify).to.eql("function");
       });
 
-      it('sends the correct params to the node-slackr library', function() {
+      it("returns a promise", function () {
         var messages = {
-            text: "using node-slackr to send messages to slack"
-        };
-
-        slack.notify(messages);
-
-        expect(slackNotify.calledWith(messages)).to.be.ok;
-      });
-
-      it('returns a promise', function() {
-        var messages = {
-            text: "I can haz promises instead of callbacks"
+          text: "I can haz promises instead of callbacks",
         };
 
         expect(slack.notify(messages).then).to.be.ok;
@@ -101,25 +81,25 @@ describe('SlackNotifier', function() {
     });
   });
 
-  describe('disabled', function() {
-    beforeEach(function() {
+  describe("disabled", function () {
+    beforeEach(function () {
       slack = new SlackNotifier({
         enabled: false,
         webhookURL: WEBHOOK_URL,
         channel: CHANNEL,
-        username: USER_NAME
+        username: USER_NAME,
       });
     });
 
-    describe('#notify', function() {
-      it('is callable', function() {
-        expect(typeof(slack.notify)).to.eql('function');
+    describe("#notify", function () {
+      it("is callable", function () {
+        expect(typeof slack.notify).to.eql("function");
       });
 
-      it('returns a promise', function() {
+      it("returns a promise", function () {
         expect(slack.notify({}).then).to.be.ok;
         expect(slack.notify({}).catch).to.be.ok;
       });
     });
   });
-})
+});
